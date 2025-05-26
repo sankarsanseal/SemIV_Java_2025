128c541441e75e53471c81f1980bc176d7b00379
Sankarsan Seal <you@example.com>
2025-05-26 10:42:45 +0530

    Commit After Remote is added

--- Java/Akshay-java/CommandArgum.java
+++ Java/Akshay-java/CommandArgum.java
@@ -5,7 +5,14 @@ public class CommandArgum {
         System.out.println("The number of argument :"+args.length);
         for(i=0;i<args.length;i++)
         {
-            System.out.println("The Argument "+ i +" : "+ args[i]);
+            System.out.println("The Argument " + i + " : " + args[i]);
+            try {
+                int number=Integer.parseInt(args[i]);
+                System.out.println("Valid Number "+number);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid Number format!");
+            }
+
         }
     }
 }
